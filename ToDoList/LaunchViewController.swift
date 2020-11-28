1fa7c82be8958cac53a1c22ff9b6d9c5f5747d68
shadowSparrow <you@example.com>
2020-11-29 01:41:36 +0300

    format1

--- ToDoList/LaunchViewController.swift
+++ ToDoList/LaunchViewController.swift
@@ -11,38 +11,19 @@ import UIKit
 class LaunchViewController: UIViewController {
 
     @IBOutlet weak var imageFish: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationController?.setNavigationBarHidden(true, animated: false)
-        
         imageFish.image = UIImage(named: "RoundedIcon")
-        
         UIView.animate(withDuration: 3, animations: {
-            
-            self.imageFish.transform = CGAffineTransform(rotationAngle: .pi)
+        self.imageFish.transform = CGAffineTransform(rotationAngle: .pi)
         }) { (Bool) in
             
             let mainStoryboard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
-            
-          let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "secondVC") as? TableViewController
-            
+            let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "secondVC") as? TableViewController
             self.navigationController?.pushViewController(secondVC!, animated: true)
-            
         }
-        
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -8,55 +8,37 @@
 
 import Foundation
 
-
 // Save changes with UserDefault
 var ToDoItems: [[String: Any]] {
     set {
         UserDefaults.standard.set(newValue, forKey: "ToDoItems")
         UserDefaults.standard.synchronize()
     }
-    
     get {
-        if let array = UserDefaults.standard.array(forKey: "ToDoItems") as? [[String:Any]]
-        {
+        if let array = UserDefaults.standard.array(forKey: "ToDoItems") as? [[String:Any]]{
             return array
-            
         } else {
             return []
         }
     }
-    
-    
 }
-    
 
 func addItem (nameItem: String, isCompleted: Bool = false) {
-    
     ToDoItems.append(["Name": nameItem, "isCompleted": isCompleted])
-    
 }
 
 func removeItem (index: Int) {
-    
     ToDoItems.remove(at: index)
-    
 }
 
 func moveRow(fromIndex: Int, toIndex: Int) {
-    
     let from = ToDoItems[fromIndex]
-    
     ToDoItems.remove(at: fromIndex)
-    
     ToDoItems.insert(from, at: toIndex)
 }
 
 func changeState(item: Int) -> Bool {
-    
     ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
-    
     return ToDoItems[item]["isCompleted"] as! Bool
-    
-    
 }
 
